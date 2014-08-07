4c79b6355c2f6e97ccd6866fd83aa1b48f058d75
Branden J Brown <zephyrtronium@gmail.com>
2014-08-07 00:46:04 -0400

    added adapters to/from math/rand Source

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package crazy
 import (
 	"crypto/rand"
 	"encoding/binary"
+	mathrand "math/rand"
 )
 
 // Compose two Sources using XOR.
@@ -24,6 +25,66 @@ func (x XorCompose) Read(p []byte) (int, error) {
 		p[i] ^= v
 	}
 	return len(p), nil
+}
+
+type math2crazy struct {
+	mathrand.Source
+}
+
+// Adapt a math/rand Source into a crazy Seeder. If the argument already
+// implements Seeder, it is returned directly. Otherwise, to preserve
+// equidistribution, the Read() method of this Seeder uses two Int63() calls
+// per eight bytes of output, using one fewer call when the output is not
+// divisible by 8. Additionally, the SeedIV() method uses only up to the first
+// eight bytes of the argument.
+func AdaptRand(src mathrand.Source) Seeder {
+	if s, ok := src.(Seeder); ok {
+		return s
+	}
+	return math2crazy{src}
+}
+
+func (m math2crazy) Read(p []byte) (n int, err error) {
+	n = len(p)
+	for len(p) >= 8 {
+		binary.LittleEndian.PutUint64(p, uint64(m.Int63()^m.Int63()<<1))
+		p = p[8:]
+	}
+	if len(p) == 8 {
+		binary.LittleEndian.PutUint64(p, uint64(m.Int63()^m.Int63()<<1))
+	} else {
+		b := [8]byte{}
+		binary.LittleEndian.PutUint64(b[:], uint64(m.Int63()))
+		copy(p, b[:])
+	}
+	return n, nil
+}
+
+func (m math2crazy) SeedIV(iv []byte) {
+	b := [8]byte{}
+	copy(b[:], iv)
+	m.Seed(int64(binary.LittleEndian.Uint64(b[:])))
+}
+
+type crazy2math struct {
+	Seeder
+}
+
+// Adapt a crazy Seeder into a math/rand Source. If the argument already
+// implements math/rand.Source, it is returned directly.
+func AdaptCrazy(src Seeder) mathrand.Source {
+	if m, ok := src.(mathrand.Source); ok {
+		return m
+	}
+	return crazy2math{src}
+}
+
+func (c crazy2math) Int63() int64 {
+	return int64(RNG{c.Seeder}.Uint64() >> 1)
+}
+
+func (c crazy2math) Seed(x int64) {
+	SeedInt64(c.Seeder, x)
 }
 
 // Create a random string with specified length using only characters from the
